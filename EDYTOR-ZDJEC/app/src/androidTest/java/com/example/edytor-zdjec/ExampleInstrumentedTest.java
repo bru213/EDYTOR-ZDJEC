5716b0e8e87f5e103a2f3aab004439de9ec7c7a8
bru213 <162710506+bru213@users.noreply.github.com>
2025-06-14 15:15:05 +0200

    Update ExampleInstrumentedTest.java

--- EDYTOR-ZDJEC/app/src/androidTest/java/com/example/edytor-zdjec/ExampleInstrumentedTest.java
+++ EDYTOR-ZDJEC/app/src/androidTest/java/com/example/edytor-zdjec/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.example.ustawienia_gry;
+package com.example.edytor-zdjec;
 
 import android.content.Context;
 
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.example.ustawienia_gry", appContext.getPackageName());
+        assertEquals("com.example.edytor-zdjec", appContext.getPackageName());
     }
 }
